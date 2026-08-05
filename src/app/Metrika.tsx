f2ea7dddd27a3c40d1876cee2a86524053b217d4
timchenkoliza <43400502+timchenkoliza@users.noreply.github.com>
2026-08-06 00:59:01 +0300

    Update Metrika.tsx

--- src/app/Metrika.tsx
+++ src/app/Metrika.tsx
@@ -5,7 +5,7 @@ export function Metrika() {
     (window as any).ym = (window as any).ym || function() {
       ((window as any).ym.a = (window as any).ym.a || []).push(arguments);
     };
-    (window as any).ym.l = Date.now();
+   (window as any).ym.l = Date.now();
     (window as any).ym(111330306, 'init', {
       ssr: true,
       webvisor: true,
